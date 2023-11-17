47a9146d4c1cf3eeb564613b97174ac83d6a184b
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2023-11-16 20:51:18 -0400

    Construido a pagina Home

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import Router from './routes/routes.jsx'
+import GlobalStyle from './style/globalStyle.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Router />
     </BrowserRouter>
+    <GlobalStyle />
   </React.StrictMode>,
 )
