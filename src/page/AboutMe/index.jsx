afb50ee65c787d387101a56a9ca3d7458f46569e
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2023-11-30 23:32:41 -0400

    iniciando a page about me

--- src/page/AboutMe/index.jsx
+++ src/page/AboutMe/index.jsx
@@ -1,8 +1,23 @@
+import { Container, Infor, Poster } from "./style"
+
 function Me(){
   return(
-    <div>
-      Hello, look! about me
-    </div>
+
+    <Container>
+      <Infor>
+        <h1>About Me</h1>
+        <p>Desenvolvedor de software apaixonado,
+          contribui com resultados, 
+          sempre buscando a melhor qualidade possível, 
+          junto comigo vem a persistência de resolver problemas que aparecem
+          Com experiência profissional desenvolvida nas habilidades das seguintes tecnologias: HTML, CSS, JavaScript e REACT. Vivência na criação de websites responsivos e interativos, proporcionando uma experiência excepcional ao usuário, contribuindo para o avanço da indústria a cada dia que passa
+          </p>
+      </Infor>
+      <Poster>
+
+      </Poster>
+    </Container>
+
   )
 }
 
